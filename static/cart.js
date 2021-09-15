0bbce08e434e29561ce47ee9a41713e8a4f0f732
Sujit Hona <suzit.hona@gmail.com>
2021-09-15 20:52:47 +0545

    checkout which sends email

--- static/cart.js
+++ static/cart.js
@@ -89,9 +89,5 @@ const updateCartTotal = () => {
     total = total + price * 1;
   }
   total = Math.round(total * 100) / 100;
-  document.getElementsByClassName("totalPrice")[0].innerText = `Rs. ${total}`;
-  let email = document.getElementById("emailsend");
-  email.value = total
-  const subtotal = document.getElementById("subtotal");
-  subtotal.innerText = cartRows.length;
-};
+  
+  
