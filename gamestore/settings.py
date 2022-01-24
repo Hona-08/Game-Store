34d3f731c6ed9fa5f93b505383f962711862612b
Sujit Hona <suzit.hona@gmail.com>
2022-01-24 22:13:37 +0545

    deployment

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9c^)4buoe)+p&(pyd1o)bqaiu9$f^w3a&sdv1_r#xt(66wkb#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'hona-gamestore.herokuapp.com']
 
 
 # Application definition
