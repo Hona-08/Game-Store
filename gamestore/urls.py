1c8d1025ba120641841abd73a7ac3478e895e52e
Sujit Hona <suzit.hona@gmail.com>
2022-01-24 22:09:03 +0545

    deployment

--- gamestore/urls.py
+++ gamestore/urls.py
@@ -19,6 +19,8 @@ from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('', include('pages.urls')),
     path('game/', include('games.urls')),
